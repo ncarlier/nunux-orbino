288f294ea04bd95538050ca1564c4ee215657432
Nicolas Carlier <n.carlier@laposte.net>
2012-07-10 16:05:45 -0400

    Update conf to be cloud ready

--- config.js
+++ config.js
@@ -3,13 +3,12 @@ module.exports = {
 	/** Server options */
 	server: {
 		/** Server port */
-		//port: process.env.PORT || 8081
-        port: 8081
+		port: process.env.PORT || 8081
 	},
 	
 	/** MQTT options */
 	mqtt: {
-		hostname: 'localhost',
+		hostname: 'test.mosquitto.org',
 		port: 1883,
         clientID: 'orbino-mobile-app'
 	},
